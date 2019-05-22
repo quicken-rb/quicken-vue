068b7fe58d0f5ffc8e2d12e92208e332039ced9b
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-05-22 16:23:01 +0200

    First commit

---
+++ lib/quicken/vue.rb
@@ -0,0 +1,8 @@
+require "quicken/vue/version"
+
+module Quicken
+  module Vue
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/quicken/vue_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Quicken::Vue do
+  it "has a version number" do
+    expect(Quicken::Vue::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
